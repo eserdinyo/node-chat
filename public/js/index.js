7c748c2bd9158ea0f895efadf0e3ea506bcd6a87
Muhammet ESER <muhammeteser29@gmail.com>
2018-09-14 19:38:00 +0300

    Add css for chat page

--- public/js/index.js
+++ public/js/index.js
@@ -9,34 +9,21 @@ socket.on('disconnect', function () {
 });
 
 socket.on('newMessage', function (message) {
-
-
-    $("#messages").append(`
-        <div class="message-box">
-            <div class="message-time">${message.createdAt}</div>
-            <div class="message-content out">
-            <p>
-                ${message.text}
-            </p>
-            </div>
-        </div>
-    `);
+    let li = $("<li></li>");
+    li.text(`${message.from}: ${message.text}`);
+    $("#messages").append(li);
 
     $('[name=message]').val("");
 });
 
 socket.on('newLocationMsg', (message) => {
-    $("#messages").append(`
-        <div class="message-box">
-            <div class="message-time">${message.from}</div>
-            <div class="message-content in">
-            <a class="my-location" href="${message.url}" target="_blank">
-                <img class="locaiton-img" src="./assets/location.png" alt="Location">
-                I'm here.
-            </a>
-            </div>
-        </div>
-    `);
+    let li = $("<li></li>");
+    let a = $("<a class='location-link' target='_blank'>I'm here</a>")
+    li.text(`${message.from}:`)
+    a.attr('href', message.url);
+    li.append(a);
+    $("#messages").append(li);
+
 });
 
 jQuery('#message-form').on('submit', (e) => {
@@ -55,12 +42,18 @@ const locationBtn = $("#send-location");
 locationBtn.on("click", () => {
     if (!navigator.geolocation)
         return alert("Geolocation not supported by your browser");
+
+    locationBtn.attr('disabled', 'disabled').text('Sending...');
+
     navigator.geolocation.getCurrentPosition((position) => {
+        locationBtn.removeAttr('disabled').text('Send Location');
+
         socket.emit("createLocationMsg", {
             lat: position.coords.latitude,
             lng: position.coords.longitude,
         })
     }, () => {
+        locationBtn.removeAttr('disabled').text('Send Location');
         alert("Unable to fetch location.");
     })
 
